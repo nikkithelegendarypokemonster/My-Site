0b12b7a183752dbaafa71d3607e0ada833238f54
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2022-02-25 05:20:59 +0800

    changed

--- js/edu.js
+++ js/edu.js
@@ -5,33 +5,39 @@ const next=document.getElementById('next');
 const edu_desc=document.getElementById("edu_desc")
 
 
-let counter=0;
+let counter=1;
 const dimension=images[0].clientWidth;
-edu_desc.innerHTML=images[0].name;
+edu_container.style.transform='translateX('+(-dimension*counter)+'px)'
+edu_desc.innerHTML=images[1].name;
 
 prev.addEventListener("click",()=>{
+  if(counter<=0) return
   edu_container.style.transition="transform 0.4s ease-in-out"
   counter--;
   edu_container.style.transform='translateX('+(-dimension*counter)+'px)'
   edu_desc.innerHTML=images[counter].name;
 })
 next.addEventListener("click",()=>{
+  if(counter>=images.length-1) return
   edu_container.style.transition="transform 0.4s ease-in-out"
   counter++;
   edu_container.style.transform='translateX('+(-dimension*counter)+'px)'
   edu_desc.innerHTML=images[counter].name;
 })
 
+
+
+
 edu_container.addEventListener("transitionend",()=>{
-  if(counter>images.length-1){
+  if(images[counter].id==="lastClone"){
     edu_container.style.transition="none"
-    counter=0;
+    counter=images.length - 2;
     edu_container.style.transform='translateX('+(-dimension*counter)+'px)'
     edu_desc.innerHTML=images[0].name;
   }
-  if(counter<0){
+  if(images[counter].id==="firstClone"){
     edu_container.style.transition="none"
-    counter=images.length-1;
+    counter=images.length-counter;
     edu_container.style.transform='translateX('+(-dimension*counter)+'px)'
     edu_desc.innerHTML=images[counter].name;
   }
